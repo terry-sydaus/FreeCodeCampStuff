0a56a411b26011e2f2012bc9dfca986de9522fc5
Terry Murphy <terryjmurphy71@gmail.com>
2019-04-18 18:50:50 +1000

    add Random Quote Machine - Reat project

---
+++ randomquotemachine/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Quote from './Quote';
+
+const App = () => {
+  return (
+    <div id='quote-box'>
+      <Header />
+      <Quote />
+    </div>
+  );
+};
+
+export default App;
