12d73901e1b70907b9b80ad378ea744d5925ae95
Terry Murphy <terryjmurphy71@gmail.com>
2019-04-18 20:51:45 +1000

    Updates

---
+++ precache-manifest.4470a8b0bc5d8f0c94e1d6de041f6202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b9c67ef3922fb6f1cdb",
+    "url": "/FreeCodeCampStuff/static/js/runtime~main.ce638520.js"
+  },
+  {
+    "revision": "a961d7b6e8059bcda7e5",
+    "url": "/FreeCodeCampStuff/static/js/main.761efbb2.chunk.js"
+  },
+  {
+    "revision": "85a9204d074aeec95736",
+    "url": "/FreeCodeCampStuff/static/js/2.69d903a1.chunk.js"
+  },
+  {
+    "revision": "a961d7b6e8059bcda7e5",
+    "url": "/FreeCodeCampStuff/static/css/main.72b65771.chunk.css"
+  },
+  {
+    "revision": "03b55111f382fed84d4a46eaf2b34967",
+    "url": "/FreeCodeCampStuff/index.html"
+  }
+];
